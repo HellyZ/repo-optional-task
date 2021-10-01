3384f440736a207c9d10f40252e004923822f6cd
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-01 22:44:37 +0300

    codereview fixes

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let num = 266219;
-let arr = Array.from(String(num), (num) => Number(num));
-console.log(arr);
-
-let result = arr.reduce((mult, current) => {
+const num = 266219;
+const result = num.toString().split('').reduce((mult, current) => {
   return mult*current
 })
 console.log(result);
-result = result**3;
-console.log(result);
+console.log(result**3);
 console.log(result.toString().slice(0,2))
