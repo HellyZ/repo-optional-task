89e0a77e851e7aa525d01a76b8543a591af4835e
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-04 12:09:52 +0300

    lesson03 optional part 1

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-const num = 266219;
-const result = num.toString().split('').reduce((mult, current) => {
-  return mult*current
-})
-console.log(result);
-console.log(result**3);
-console.log(result.toString().slice(0,2))
+let lang;
+const ru = `
+  Понедельник\n
+  Вторник\n
+  Среда\n
+  Четверг\n
+  Пятница\n
+  Суббота\n
+  Воскресенье\n`;
+const en = `
+  Monday\n
+  Tuesday\n
+  Wednesday\n
+  Thursday\n
+  Friday\n
+  Saturday\n
+  Sunday`
+lang = prompt("Укажите язык");
+
+console.log("======== if =========");
+if (lang == "ru") {
+  console.log(ru);
+} else if (lang == "en") {
+  console.log(en);
+} else {
+  console.log("error");
+}
+
+console.log("======== switch-case =========");
+
+switch (lang) {
+  case "ru":
+    console.log(ru);
+    break;
+  case "en":
+    console.log(en);
+    break;
+}
+
+console.log("======== многомерный массив =========");
+
+const week = [[ru],[en]]
+console.log((lang == "ru")? week[0].toString() : week[1].toString());
