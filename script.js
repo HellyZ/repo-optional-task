bfcec4bc64e2431026401ba40248eaace39f0dab
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-05 23:00:02 +0300

    refactor type check

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 "use strict";
 
 const inputData = prompt("Введите строку: ");
+const isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+  };
 const DEFAULT_MAX_LENGTH = 30;
 
 function parsString(input, maxLength) {
-  if (isNaN(input)) {
+  if (typeof input === 'string' && !isNumber(input)) {
     const str = input.trim();
     const result =
       str.length > maxLength ? `${str.substring(0, maxLength)} ...` : str;
--- script2nd.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let namePerson = prompt("Введите имя пользователя: ");
-
-namePerson.toLowerCase() == "артем" || "артём"
-  ? console.log("директор")
-  : namePerson.toLowerCase() == "александр"
-  ? console.log("преподаватель")
-  : console.log("студент");
