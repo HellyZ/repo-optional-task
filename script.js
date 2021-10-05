5f36af35503fe02fc332f09f1823912582fb961b
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-05 22:20:33 +0300

    fix isNaN check

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const inputData = prompt("Введите строку: ");
 const DEFAULT_MAX_LENGTH = 30;
 
 function parsString(input, maxLength) {
-  if (typeof input === "string") {
+  if (isNaN(input)) {
     const str = input.trim().trimLeft();
     const result =
       str.length > maxLength ? `${str.substring(0, maxLength)} ...` : str;
