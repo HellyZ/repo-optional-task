7943dc50da6acd5736bcd55cffd50338d26f5f4f
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-05 21:32:58 +0300

    4 optional task

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-let lang;
-const ru = `
-  Понедельник\n
-  Вторник\n
-  Среда\n
-  Четверг\n
-  Пятница\n
-  Суббота\n
-  Воскресенье\n`;
-const en = `
-  Monday\n
-  Tuesday\n
-  Wednesday\n
-  Thursday\n
-  Friday\n
-  Saturday\n
-  Sunday`;
-lang = prompt("Укажите язык");
+"use strict";
+const inputData = prompt("Введите строку: ");
 
-console.log("======== if =========");
-if (lang == "ru") {
-  console.log(ru);
-} else if (lang == "en") {
-  console.log(en);
-} else {
-  console.log("error");
+function parsString(input) {
+  if (typeof input === "string") {
+    let str = input.trim().trimLeft();
+    if (str.length > 30){
+      console.log(">30");
+      console.log(str.substr(0,30) + "...");
+      return str.substr(0,30) + "...";
+    } else {
+      console.log("<30");
+      console.log(str);
+      return str;
+    }
+  } else {
+    console.log("Вводные данные не являются строкой!");
+    return null;
+  }
 }
 
-console.log("======== switch-case =========");
-
-switch (lang) {
-  case "ru":
-    console.log(ru);
-    break;
-  case "en":
-    console.log(en);
-    break;
-}
-
-console.log("======== многомерный массив =========");
-
-const week = [[ru], [en]];
-console.log(lang == "ru" ? week[0].toString() : week[1].toString());
+parsString(inputData);
