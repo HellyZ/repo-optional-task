54ec296ebfbafcb7d3a42eb7d4c1b0b5263bb155
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-06 20:50:43 +0300

    optional task 1

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 "use strict";
+const MIN = 10;
+const MAX = 1000;
+const arr = ["485", "258"];
 
-const inputData = prompt("Введите строку: ");
-const isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-  };
-const DEFAULT_MAX_LENGTH = 30;
-
-function parsString(input, maxLength) {
-  if (typeof input === 'string' && !isNumber(input)) {
-    const str = input.trim();
-    const result =
-      str.length > maxLength ? `${str.substring(0, maxLength)} ...` : str;
-    console.log(result);
-    return result;
-  } else {
-    console.log("Вводные данные не являются строкой!");
-    return null;
+function fullArray(min, max) {
+  for (let i = 0; i < 5; i++) {
+    arr.push((Math.floor(Math.random() * (max - min + 1)) + min).toString());
   }
+  console.log("fullArray: " + arr);
+  return arr;
 }
 
-parsString(inputData, DEFAULT_MAX_LENGTH);
+function printNumbers(arr) {
+  let result = arr.filter((i) => i[0] === "2" || i[0] === "4");
+  return result;
+}
+
+console.log("Result: " + printNumbers(fullArray(MIN, MAX)));
