0d622d08219015031945776bf75d911e7de650ac
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-06 21:01:07 +0300

    optional task 2

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 "use strict";
 
-const inputData = prompt("Введите строку: ");
-const isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-  };
-const DEFAULT_MAX_LENGTH = 30;
+const MIN = 1;
+const MAX = 100;
 
-function parsString(input, maxLength) {
-  if (typeof input === 'string' && !isNumber(input)) {
-    const str = input.trim();
-    const result =
-      str.length > maxLength ? `${str.substring(0, maxLength)} ...` : str;
-    console.log(result);
-    return result;
-  } else {
-    console.log("Вводные данные не являются строкой!");
-    return null;
+function getPrimes(min, max) {
+  for (let i = min; i <= max; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        flag = 1;
+        break;
+      }
+    }
+
+    if (i > 1 && flag == 0) {
+      console.log(`Делители этого числа: 1 и ${i}`);
+    }
   }
 }
 
-parsString(inputData, DEFAULT_MAX_LENGTH);
+console.log(getPrimes(MIN, MAX));
