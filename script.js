860d62dd257d4e80bcef693a225c01362fafe12a
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-12 23:36:28 +0300

    optional task

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 "use strict";
 
-const body = document.querySelector("body");
+const fullDate = document.getElementById("fullDate");
+const shortDate = document.getElementById("shortDate");
 
-const week = {
+const SECONDS_FORM = ["секунда", "секунды", "секунд"];
+const MINUTES_FORM = ["минута", "минуты", "минут"];
+const HOURS_FORM = ["час", "часа", "часов"];
+
+const timeForm = function (number, forms) {
+  return forms[
+    number % 100 > 4 && number % 100 < 2
+      ? 2
+      : [2, 0, 1, 1, 1, 2][number % 10 < 5 ? Math.abs(number) % 10 : 5]
+  ];
+};
+
+const date = {
+  now: Date(),
   days: [
     "Понедельник",
     "Вторник",
@@ -12,22 +26,71 @@ const week = {
     "Суббота",
     "Воскресенье",
   ],
-  getWeek: function () {
-    week.doCursive();
-    week.makeBoldToday();
-    body.innerHTML = week.days.join("<br/>");
+  monthes: [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Майя",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря",
+  ],
+  nowDate: function () {
+    date.now = new Date();
+    return date.now;
   },
-
-  doCursive: function () {
-    week.days = week.days
-      .slice(0, -2)
-      .concat(week.days.slice(5).map((i) => `<i>${i}</i>`));
+  formatterShortDate: function () {
+    let tempDate = date.nowDate();
+    return (
+      [
+        ("00" + tempDate.getDate()).slice(-2),
+        ("00" + (tempDate.getMonth() + 1)).slice(-2),
+        ("00" + tempDate.getFullYear()).slice(-2),
+      ].join(".") +
+      " - " +
+      [
+        ("00" + tempDate.getHours()).slice(-2),
+        ("00" + tempDate.getMinutes()).slice(-2),
+        ("00" + tempDate.getSeconds()).slice(-2),
+      ].join(":")
+    );
   },
-  makeBoldToday: function () {
-    const today = new Date(Date());
-    let weekday = today.getDay();
-    weekday--;
-    week.days[weekday] = `<b>${week.days[weekday]}</b>`;
+  formatterLongDate: function () {
+    let tempDate = date.nowDate();
+    let datetimestring = `Сегодня ${
+      date.days[tempDate.getDay() - 1]
+    }, ${tempDate.getDate()} ${
+      date.monthes[tempDate.getMonth()]
+    } ${tempDate.getFullYear()} года, 
+    ${tempDate.getHours()} ${timeForm(
+      tempDate.getHours(),
+      HOURS_FORM
+    )} ${tempDate.getMinutes()} ${timeForm(
+      tempDate.getMinutes(),
+      MINUTES_FORM
+    )} ${tempDate.getSeconds()} ${timeForm(
+      tempDate.getSeconds(),
+      SECONDS_FORM
+    )}`;
+    return datetimestring;
+  },
+  calculateDate: function () {
+    fullDate.innerHTML = date.formatterLongDate();
+    shortDate.innerHTML = date.formatterShortDate();
+  },
+  displayDate: function () {
+    setInterval(function () {
+      date.calculateDate();
+    }, 1000);
+  },
+  start: function () {
+    date.displayDate();
   },
 };
-week.getWeek();
+
+date.start();
